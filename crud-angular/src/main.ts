669647deaf2ada98f6c98106b570772fe0b85549
JulianCambraia <juliancambraia@gmail.com>
2024-04-23 22:56:20 -0300

    migração para standalone components, remove rotas desnecessárias

--- crud-angular/src/main.ts
+++ crud-angular/src/main.ts
@@ -1,25 +1,34 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  BrowserModule,
+  bootstrapApplication,
+  provideClientHydration,
+} from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideClientHydration, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule),
-        provideClientHydration(),
-        provideAnimationsAsync(),
-        provideHttpClient(withFetch()),
-        provideClientHydration(),
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi()),
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(BrowserModule, MatToolbarModule),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+  ],
+}).catch((err) => console.error(err));
