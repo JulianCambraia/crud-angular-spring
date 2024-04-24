c47bfcfb51218dd2d48b5214025104df8aafa460
JulianCambraia <juliancambraia@gmail.com>
2024-04-23 22:10:23 -0300

    migração para APIs standalone

--- crud-angular/src/main.ts
+++ crud-angular/src/main.ts
@@ -1,7 +1,25 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule),
+        provideClientHydration(),
+        provideAnimationsAsync(),
+        provideHttpClient(withFetch()),
+        provideClientHydration(),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi()),
+    ]
+})
   .catch(err => console.error(err));
